fd433045e80a87b224fa75c263f8f786caa74a63
Murat OCAK <mnoktaonokta@hotmail.com>
2020-05-28 23:41:10 +0300

    ilk açılış

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by EYMENKO on 28.05.2020.
+//  Copyright © 2020 muratocak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
